20b2316e2a5539b54e5da208ce87444f3580a90c
Vincent Bernardoff <vb@luminar.eu.org>
2021-10-28 10:27:17 +0800

    add LICENSE

--- lib/simdjson.ml
+++ lib/simdjson.ml
@@ -1,3 +1,9 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2021 The ocaml-simdjson programmers. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+   %%NAME%% %%VERSION%%
+  ---------------------------------------------------------------------------*)
+
 type p
 type e
 type o
@@ -228,3 +234,19 @@ let arr_exn f {e; gen} =
       let e = arrayIteratorGet iter in
       arrayIteratorNext iter ;
       f i {e; gen} )
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2021 The ocaml-simdjson programmers
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
